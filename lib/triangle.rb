e4534c10701f049f845bd9f10a2a52dd0050c43a
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-24 16:18:51 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,7 @@
 require "pry"
 class Triangle
   attr_accessor :s1,:s2,:s3
+
   def initialize(s1,s2,s3)
     @s1 = s1
     @s2 = s2
@@ -25,6 +26,7 @@ def valid?
   valid_triangle_check = [(s1 + s2 > s3),(s2 + s3 > s1),(s3+s1 > s2), (s1 <= 0), (s2 <= 0), (s3 <= 0)]
     raise TriangleError if valid_triangle_check.include?(false)
   end
+end
 
 class TriangleError < StandardError
 end
