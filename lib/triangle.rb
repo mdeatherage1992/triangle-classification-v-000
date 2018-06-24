40497c515f4f70793cec2530a57723ff02456258
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-24 16:13:51 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,13 +10,10 @@ class Triangle
   def kind
   end
 
-
-
 def valid?
   valid_triangle_check = [(s1 + s2 > s3),(s2 + s3 > s1),(s3+s1 > s2), (s1 <= 0), (s2 <= 0), (s3 <= 0)]
     raise TriangleError if valid_triangle_check.include?(false)
   end
-
 
 class TriangleError < StandardError
 end
