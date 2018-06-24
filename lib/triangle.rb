78bf07929fc933b8d1555f0071109c7f24d8240a
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-24 16:13:28 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,17 +1,10 @@
 require "pry"
 class Triangle
   attr_accessor :side_1, :side_2, :side_3
-  @@count = 0
   def initialize(s1,s2,s3)
     @s1 = s1
     @s2 = s2
     @s3 - s3
-  end
-
-  def count
-    triangle.each do |side|
-      @@count += side
-    end
   end
 
   def kind
