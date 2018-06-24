e139d71813d47331bdee74b6738fb6ba70578d9b
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-24 16:24:15 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,7 +21,7 @@ class Triangle
 
 
 def valid?
-  valid_triangle_check = [(s1 + s2 > s3),(s2 + s3 > s1),(s3+s1 > s2), (s1 <= 0), (s2 <= 0), (s3 <= 0)]
+  valid_triangle_check = [(s1 + s2 > s3),(s2 + s3 > s1),(s3 + s1 > s2), (s1 <= 0), (s2 <= 0), (s3 <= 0)]
     raise TriangleError if valid_triangle_check.include?(false)
   end
 
