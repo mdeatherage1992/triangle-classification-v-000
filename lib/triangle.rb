611fe951ab6ffac92d2072435f93b4a11eac8b67
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-24 16:09:09 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,10 +2,10 @@ require "pry"
 class Triangle
   attr_accessor :side_1, :side_2, :side_3
   @@count = 0
-  def initialize(side_1,side_2,side_3)
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
+  def initialize(s1,s2,s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 - s3
   end
 
   def count
@@ -14,27 +14,12 @@ class Triangle
     end
   end
 
+  def kind
 
 
-  def kind(triangle)
-    self.triangle = triangle
-    triangle.each do |side|
-    if side < 1 || side > @@count - side
-      begin
-         raise TriangleError
-       rescue TriangleError => error
-           puts error.message
-       end
-     else
-       true
-  class TriangleError < StandardError
-    def message
-      "illegal!"
-    end
-  end
-end
-end
-end
 
+def valid?
+  valid_triangle_check = [(side_1 + side_2 > side_3),(side_2) ]
 
+class TriangleError < StandardError
 end
